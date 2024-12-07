ee1ccd4a8ccf74942d726a458c2e1b37db713e46
r1a2m3prasath <ramcoc37@gmail.com>
2024-12-07 13:12:53 +0530

    push

---
+++ leap.java
@@ -0,0 +1,16 @@
+class leap 
+{
+public static void main(String args[])
+	{
+		int a = 2005;
+		System.out.println(a);
+		if(a%4==0)
+			{
+			System.out.println("its leap");
+			}
+		else
+			{
+			System.out.println("its not leap");
+			}
+	}
+}
